c31d683cbc8d1e1b3cf7849dc678cdcdc4a69783
Florence-Njeri <flonsie17@gmail.com>
2019-12-12 22:20:54 +0300

    add dummy data to recyclerview

--- app/src/main/java/com/example/moviesapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.example.moviesapp.R
 
 class HomeFragment : Fragment() {
@@ -22,9 +23,9 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        val moviesList: RecyclerView = root.findViewById(R.id.movies_recyclerView)
         homeViewModel.text.observe(this, Observer {
-            textView.text = it
+//            moviesList.text = it
         })
         return root
     }
