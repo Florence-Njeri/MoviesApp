53c591267c7c95cffeaeb5e53383157472985a71
Florence-Njeri <flonsie17@gmail.com>
2019-12-13 16:43:49 +0300

    add data binding and movies adapter

---
+++ app/src/main/java/com/example/moviesapp/adapter/MoviesAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.moviesapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviesapp.R
+
+class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder>() {
+
+    //TODO: Add a list from the data class
+//    val moviesData= listOf<>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
+        var layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.movie_item, parent, false)
+        return MoviesViewHolder(view)
+    }
+
+    override fun getItemCount() = 12
+    override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
+        holder.bind()
+
+    }
+
+    class MoviesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var movieImage: ImageView = view.findViewById(R.id.movieImage)
+        var movieTitle: TextView = view.findViewById(R.id.movieTitle)
+        var releaseYear: TextView = view.findViewById(R.id.releaseYear)
+        var ratingBar: RatingBar = view.findViewById(R.id.ratingBar)
+
+        fun bind() {
+            movieImage.setImageResource(R.drawable.aquaman)
+            movieTitle.text = "Aquaman"
+            releaseYear.text = "2018"
+            ratingBar.numStars= 3.5.toInt()
+        }
+    }
+}
--- app/src/main/java/com/example/moviesapp/ui/movies/MoviesFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/movies/MoviesFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.moviesapp.R
+import com.example.moviesapp.adapter.MoviesAdapter
+import com.example.moviesapp.databinding.FragmentMoviesBinding
 
 class MoviesFragment : Fragment() {
 
     private lateinit var moviesViewModel: MoviesViewModel
+    private lateinit var binding: FragmentMoviesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,8 +23,18 @@ class MoviesFragment : Fragment() {
     ): View? {
         moviesViewModel =
             ViewModelProviders.of(this).get(MoviesViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_movies, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movies, container, false)
+
         //RecyclerView to be added
-        return root
+        val adapter=MoviesAdapter()
+
+        binding.moviesRecyclerView.adapter=adapter
+        //TODO: Change from text to recyclerview
+//        moviesViewModel.list.observe(viewLifecycleOwner, Observer {
+//            it?.let {
+////                adapter.data= it
+//            }
+//        })
+        return binding.root
     }
 }
