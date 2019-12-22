efb4983a3293fc22bb07590cc401fcc65877cac0
Florence-Njeri <flonsie17@gmail.com>
2019-12-22 17:15:15 +0300

    refactor movie details scroll to parallax scrolling

--- app/src/main/java/com/example/moviesapp/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/moviesapp/MovieDetailsActivity.kt
@@ -3,17 +3,21 @@ package com.example.moviesapp
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.moviesapp.databinding.ActivityMovieDetailsBinding
 import kotlinx.android.synthetic.main.activity_movie_details.*
 
 class MovieDetailsActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMovieDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_movie_details)
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        binding.collapsingToolbar.title="Movie Details"
     }
 }
