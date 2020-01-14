104ee5cb1a861551a2a84b84f0b6f88e9e8dafc0
Florence-Njeri <flonsie17@gmail.com>
2020-01-14 15:45:15 +0300

    resolve git conflict

--- app/src/main/java/com/example/moviesapp/ui/series/SeriesFragment.kt
+++ app/src/main/java/com/example/moviesapp/ui/series/SeriesFragment.kt
@@ -14,20 +14,15 @@ import com.example.moviesapp.ui.comedy.SeriesViewModel
 
 class SeriesFragment : Fragment() {
 
-    private lateinit var seriesViewModel: SeriesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        seriesViewModel =
-            ViewModelProviders.of(this).get(SeriesViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_series, container, false)
-        val textView: TextView = root.findViewById(R.id.text_series)
-        seriesViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
         return root
     }
 }
